b62e68288dbabb3ba08f3b931af71b7f7eb525a4
Andre <andrersfcosta@gmail.com>
2025-04-13 19:42:53 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://andrersfc.github.io/waiting-time-perception-web/sw.js', { scope: 'https://andrersfc.github.io/waiting-time-perception-web/' })})}
